0d161ec528cfa6f29e29327fb6e883f293690f31
ElisOre <elias.orellana.31@gmail.com>
2023-11-04 10:32:29 -0300

    feat: Crear ruta raíz

--- app.js
+++ app.js
@@ -7,3 +7,7 @@ const publicPath = path.join(__dirname, "public");
 app.use(express.static(publicPath));
 
 app.listen(3000, () => console.log("listening on port 3000"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/index.html"));
+});
