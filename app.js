f1533f975fb46637d9f3a57e97cef509af760508
matiasgomez781 <darksho781@gmail.com>
2023-11-15 22:09:02 -0300

    Creacion de rama detail y productDetail.html + ruteo en app.js

--- app.js
+++ app.js
@@ -7,3 +7,8 @@ const publicPath = path.join(__dirname, "public");
 app.use(express.static(publicPath));
 
 app.listen(3000, () => console.log("listening on port 3000"));
+
+//Ruta Product detail
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, 'views/productDetail.html'));
+});
