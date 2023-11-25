4715eaee4514410798e55e232aee89e863a05e06
ElisOre <elias.orellana.31@gmail.com>
2023-11-24 21:13:00 -0300

    feat: implementar patrón de diseño MVC

--- src/app.js
+++ src/app.js
@@ -3,9 +3,7 @@ const path = require("path");
 
 const app = express();
 
-const publicPath = path.join(__dirname, "public");
-
-app.use(express.static(publicPath));
+app.use(express.static("public"));
 
 app.listen(3000, () => console.log("listening on port 3000"));
 
