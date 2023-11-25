6fe2965a53e442e4843c8b27f1d355e380f410f7
ElisOre <elias.orellana.31@gmail.com>
2023-11-25 17:12:20 -0300

    feat: división de tareas con controller y router

--- src/app.js
+++ src/app.js
@@ -1,29 +1,27 @@
 const express = require("express");
-const path = require("path");
-
 const app = express();
+
+const mainRouter = require("./routes/main.routes");
 
 app.use(express.static("public"));
 
 app.listen(3000, () => console.log("listening on port 3000"));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views/index.html"));
-});
+app.use("/", mainRouter);
 
 //Ruta Carrito
 app.get("/productCart", (req, res) => {
   res.sendFile(path.join(__dirname, "views/productCart.html"));
 });
-app.get ('/registro', (req,res) =>{
-    res.sendFile ( path.resolve(__dirname, './views/registro.html'))
+app.get("/registro", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/registro.html"));
 });
 
 app.get("/loggin", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/loggin.html"));
+  res.sendFile(path.resolve(__dirname, "./views/loggin.html"));
 });
 
 //Ruta Product detail
-app.get('/detail', (req,res) => {
-    res.sendFile(path.join(__dirname, 'views/productDetail.html'));
+app.get("/detail", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/productDetail.html"));
 });
