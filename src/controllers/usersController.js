5e28f1f2e008af0501836e02bf9946ac597ab69f
leandro22371@gmail.com <leandro22371@gmail.com>
2023-12-02 21:33:57 -0300

    fix: header, form

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-const path = require ('path');
-const product =require('../model/product')
-
-let usersController = {
-    
-    // index: (req,res)=>{
-    //     res.render('index',{element: user.getAllUsers()})
-    // },
-
-    login: (req,res)=>{
-        res.render('login')
-    },
-
-    registro: (req,res)=>{
-        res.render('registro')
-    },
-
-    getOne: (req,res)=>{
-        res.send('pagina del user' + req.params.id)
-    },
-
-    
-
-}
-=======
 const path = require("path");
 
 const usersController = {
@@ -34,7 +8,9 @@ const usersController = {
   register: (req, res) => {
     res.render(("./users/registro"));
   },
+  getOne: (req,res)=>{
+    res.send('pagina del user' + req.params.id)
+  }
 };
->>>>>>> 10bbbab13731c6299bf19fa4327f8302b6fd734d
 
 module.exports = usersController;
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,22 +1,11 @@
-<<<<<<< HEAD
 const express = require ('express');
 const router = express.Router();
 const usersController = require ('../controllers/usersController')
 
 router.get('/', usersController.login)
 
-router.get('/registro', usersController.registro)
+router.get('/registro', usersController.register)
 
 router.get ('/:id', usersController.getOne)
-=======
-const express = require("express");
-const router = express.Router();
-const usersController = require("../controllers/usersController");
-
-//Ruta Raiz => Login
-router.get ("/login", usersController.login);
-router.get ("/registro", usersController.register);
-
->>>>>>> 10bbbab13731c6299bf19fa4327f8302b6fd734d
 
 module.exports = router;
