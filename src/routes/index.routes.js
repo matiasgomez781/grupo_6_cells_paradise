fa079b3953bfa73f4fa25dc8fa8ef193ef2af5de
ElisOre <elias.orellana.31@gmail.com>
2023-12-03 20:01:52 -0300

    fix: width de slides

--- src/routes/index.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require ('express');
-const router = express.Router();
-const routerUser = require ('./users.routes');
-
-router.use('/usuarios', routerUser)
-
-module.exports = router;
