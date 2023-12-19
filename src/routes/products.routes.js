12049bd17bd75376b5076de869bb14ad654b45b8
ElisOre <elias.orellana.31@gmail.com>
2023-12-18 23:33:34 -0300

    feat: rutas creadas

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -3,13 +3,16 @@ const router = express.Router();
 
 const productsController = require("../controllers/productsController");
 
-router.get("/", productsController.all)
+router.get("/", productsController.all);
 
 router.get("/detail/:id", productsController.detail);
 
 router.get("/createProduct", productsController.createProduct);
+router.post("/products");
 
-router.get("/editProduct", productsController.editProduct);
+router.get("/:id/editProduct", productsController.editProduct);
+router.put("/products/:id");
 
+router.delete("/products/:id");
 
 module.exports = router;
