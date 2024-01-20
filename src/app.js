c2df106b2efe8b1631fabdc731c062b83e31e18e
matiasgomez781 <darksho781@gmail.com>
2024-01-19 23:01:04 -0300

    log Middleware, y error 404 middleware global

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const app = express();
 const routerlogin = require("./routes/users.routes");
 const mainRouter = require("./routes/main.routes");
 const productsRouter = require("./routes/products.routes");
+const logMiddleware = require("./middlewares/logMiddleware");
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use(methodOverride("_method")); // Pasar poder pisar el method="POST" en el formulario por PUT y DELETE
 app.use(session({ secret: "Secret message" }));
 app.use(cookieParser());
+app.use(logMiddleware);
 
 app.listen(3000, () => console.log("listening on port 3000"));
 
@@ -25,3 +27,9 @@ app.set("views", path.join(__dirname, "views"));
 app.use("/", mainRouter);
 app.use("/users", routerlogin);
 app.use("/products", productsRouter);
+
+//Error 404
+
+app.use((req,res, next) => {
+    res.status(404).render("./main/error-404");
+})
