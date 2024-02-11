22506212aacab69ea0e7bb383cdb023dcdb19512
matiasgomez781 <darksho781@gmail.com>
2024-02-11 10:45:05 -0300

    Asociaciones para modelo Adress.js

--- src/model/database/models/Address.js
+++ src/model/database/models/Address.js
@@ -22,5 +22,18 @@ module.exports = (sequelize, DataTypes) => {
     };
     let Address = sequelize.define(alias, columns, config);
 
+    Address.associate = function(models) {
+        Address.belongsTo(models.PostalCode, {
+            as: 'postal_code',
+            foreignKey: 'id_postal_code'
+        });
+        Address.belongsToMany(models.User, {
+            as: 'users',
+            through: 'user_address',
+            foreignKey: 'id_address',
+            otherKey: 'id_user'
+        });
+    };
+
     return Address;
 };
