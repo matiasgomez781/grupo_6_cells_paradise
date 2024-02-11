687517a06e7a802391d21b89c8447881b9306d4a
matiasgomez781 <darksho781@gmail.com>
2024-02-10 22:26:02 -0300

    Asociaciones para Modelo Product.js segun tabla sql

--- src/model/database/models/Product.js
+++ src/model/database/models/Product.js
@@ -38,5 +38,48 @@ module.exports = (sequelize, DataTypes) => {
     };
     let Product = sequelize.define(alias, columns, config);
 
+    Product.associate = function(models) {
+        Product.belongsTo(models.Category, {
+            as: 'category',
+            foreignKey: 'id_category'
+        });
+        Product.belongsTo(models.Brand, {
+            as: 'brand',
+            foreignKey: 'id_brand'
+        });
+        Product.hasMany(models.Image, {
+            as: 'images',
+            foreignKey: 'id_product'
+        });
+        Product.belongsToMany(models.Color, {
+            as: 'colors',
+            through: 'product_color',
+            foreignKey: 'id_product',
+            otherKey: 'id_color'
+        });
+        Product.belongsToMany(models.Cart, {
+            as: 'carts',
+            through: 'product_cart',
+            foreignKey: 'id_product',
+            otherKey: 'id_cart'
+        });
+        Product.belongsToMany(models.Sale, {
+            as: 'sales',
+            through: 'product_sale',
+            foreignKey: 'id_product',
+            otherKey: 'id_sale'
+        });
+        Product.belongsToMany(models.Purchase, {
+            as: 'purchases',
+            through: 'product_purchase',
+            foreignKey: 'id_product',
+            otherKey: 'id_purchase'
+        });
+        Product.hasOne(models.Stock, {
+            as: 'stock',
+            foreignKey: 'id_product'
+        });
+    };
+
     return Product;
 };
