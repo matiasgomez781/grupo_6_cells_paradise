c342dea2a828083f3b1ff34c19c83b4ee64b451d
matiasgomez781 <darksho781@gmail.com>
2024-02-12 19:52:24 -0300

    Configurar las opciones de session en app.js (resave, saveUninitialized)

--- src/app.js
+++ src/app.js
@@ -15,7 +15,11 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(methodOverride("_method")); // Pasar poder pisar el method="POST" en el formulario por PUT y DELETE
-app.use(session({ secret: "Secret message" }));
+app.use(session({
+    secret: 'Mensaje secreto', // Esto debería ser un valor único y seguro en un entorno de producción
+    resave: false, // Evita que la sesión se guarde en el almacenamiento si no hay cambios
+    saveUninitialized: false // Evita guardar sesiones que aún no se han inicializado
+  }));
 app.use(cookieParser());
 app.use(loginMid);
 
