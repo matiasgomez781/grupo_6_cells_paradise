4499bd1aab2b3a21ce18d27a18ca6f547302de8e
MaTuTe248 <matutegnunez@com>
2024-02-26 23:26:07 -0300

    creando validation de registro

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -9,14 +9,14 @@ const validationsLogin= require('../middlewares/validationLoginMid');
 
 // Cambiar la ruta a /login
 router.get("/login", authMid, usersController.login);
-router.post('/login', validationsLogin,usersController.loginProcess);
+router.post('/login',usersController.loginProcess);
 
 router.post("/logout", usersController.logout);
 
 router.get("/profile/:id", usersController.detail);
 
 router.get("/register", authMid, usersController.register);
-router.post("/", upload.single("avatar"), validationsRegister,usersController.newUser);
+router.post("/", upload.single("avatar"), usersController.newUser);
 
 
 router.get('/edit/:id', usersController.edit);
