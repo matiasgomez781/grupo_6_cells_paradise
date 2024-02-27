935c01ef0f520b779d63c2891467c0a4f795c5ac
ElisOre <elias.orellana.31@gmail.com>
2024-02-26 22:40:51 -0300

    fix: email debe ser único

--- src/model/database/models/User.js
+++ src/model/database/models/User.js
@@ -1,69 +1,70 @@
 module.exports = (sequelize, DataTypes) => {
-    let alias = 'User';
-    let columns = {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        id_rol: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        first_name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        last_name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        phone: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        avatar: {
-            type: DataTypes.TEXT,
-            allowNull: false
-        },
-        birth_date: {
-            type: DataTypes.DATE,
-            allowNull: false
-        },
-        dni: {
-            type: DataTypes.STRING,
-            allowNull: false
-        }
-    };
-    let config = {
-        tableName: 'users',
-        timestamps: false
-    };
-    let User = sequelize.define(alias, columns, config);
+  let alias = "User";
+  let columns = {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    id_rol: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    avatar: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    birth_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    dni: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  };
+  let config = {
+    tableName: "users",
+    timestamps: false,
+  };
+  let User = sequelize.define(alias, columns, config);
 
-    User.associate = function(models) {
-        User.belongsTo(models.Role, {
-            as: 'role',
-            foreignKey: 'id_rol'
-        });
-        User.hasMany(models.Address, {
-            as: 'addresses',
-            foreignKey: 'id_user'
-        });
-        User.hasMany(models.Cart, {
-            as: 'carts',
-            foreignKey: 'id_user'
-        });
-        User.hasMany(models.Purchase, {
-            as: 'purchases',
-            foreignKey: 'id_user'
-        });
-    };
+  User.associate = function (models) {
+    User.belongsTo(models.Role, {
+      as: "role",
+      foreignKey: "id_rol",
+    });
+    User.hasMany(models.Address, {
+      as: "addresses",
+      foreignKey: "id_user",
+    });
+    User.hasMany(models.Cart, {
+      as: "carts",
+      foreignKey: "id_user",
+    });
+    User.hasMany(models.Purchase, {
+      as: "purchases",
+      foreignKey: "id_user",
+    });
+  };
 
-    return User;
+  return User;
 };
