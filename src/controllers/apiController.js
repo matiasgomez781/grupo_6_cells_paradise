bec3f02d9c8be594e84beafd244a9fed36552790
ElisOre <elias.orellana.31@gmail.com>
2024-03-15 10:22:36 -0300

    feat: rutas y controlador de las api lista. faltan modificaciones

---
+++ src/controllers/apiController.js
@@ -0,0 +1,48 @@
+const productService = require("../model/services/productService");
+const userService = require("../data/usersService");
+
+module.exports = {
+  users: async (req, res) => {
+    try {
+      let users = await userService.getAll();
+
+      res.status(200).json(users);
+    } catch (error) {
+      console.log(error.message);
+      return [];
+    }
+  },
+
+  userDetail: async (req, res) => {
+    try {
+      let user = await userService.getById(req.params.id);
+
+      res.status(200).json(user);
+    } catch (error) {
+      console.log(error.message);
+      return [];
+    }
+  },
+
+  products: async (req, res) => {
+    try {
+      let products = await productService.getAll();
+
+      res.status(200).json(products);
+    } catch (error) {
+      console.log(error.message);
+      return [];
+    }
+  },
+
+  productDetail: async (req, res) => {
+    try {
+      let product = await productService.getOne(req.params.id);
+
+      res.status(200).json(product);
+    } catch (error) {
+      console.log(error.message);
+      return [];
+    }
+  },
+};
