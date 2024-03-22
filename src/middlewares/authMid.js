e713c832420ac0ca8c896cb591ae51ff361edc7e
ElisOre <elias.orellana.31@gmail.com>
2024-03-22 05:32:43 -0300

    chore: borro logs innecesarios

--- src/middlewares/authMid.js
+++ src/middlewares/authMid.js
@@ -1,8 +1,6 @@
 let authMid = async (req, res, next) => {
   const userLogged = await req.session.userLogged;
   if (userLogged) {
-    console.log("SESSIONNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN");
-    console.log(userLogged);
     return res.redirect("/users/profile/" + userLogged.id);
   }
 
