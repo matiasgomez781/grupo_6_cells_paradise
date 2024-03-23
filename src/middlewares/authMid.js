7b522dfd16b7cfc6463d53fd6f6fa75f4ce22ee5
ElisOre <elias.orellana.31@gmail.com>
2024-03-23 03:21:40 -0300

    fix: haciendo async a middlewres para no crear conflictos

--- src/middlewares/authMid.js
+++ src/middlewares/authMid.js
@@ -1,7 +1,12 @@
 let authMid = async (req, res, next) => {
-  const userLogged = await req.session.userLogged;
-  if (userLogged) {
-    return res.redirect("/users/profile/" + userLogged.id);
+  try {
+    const userLogged = await req.session.userLogged;
+    if (userLogged) {
+      return res.redirect("/users/profile/" + userLogged.id);
+    }
+  } catch (error) {
+    console.log(error.message);
+    return next(error);
   }
 
   next();
