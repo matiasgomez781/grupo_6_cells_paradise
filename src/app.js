4052779fc7d8d43f17bddc3e377bc34560431c6f
ElisOre <elias.orellana.31@gmail.com>
2024-03-25 20:23:58 -0300

    feat: agrego cors

--- src/app.js
+++ src/app.js
@@ -13,13 +13,28 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(methodOverride("_method")); // Pasar poder pisar el method="POST" en el formulario por PUT y DELETE
-app.use(session({
-    secret: 'Mensaje secreto', // Esto debería ser un valor único y seguro en un entorno de producción
+app.use(
+  session({
+    secret: "Mensaje secreto", // Esto debería ser un valor único y seguro en un entorno de producción
     resave: false, // Evita que la sesión se guarde en el almacenamiento si no hay cambios
-    saveUninitialized: false // Evita guardar sesiones que aún no se han inicializado
-  }));
+    saveUninitialized: false, // Evita guardar sesiones que aún no se han inicializado
+  })
+);
 app.use(cookieParser());
 app.use(loginMid);
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
 
 app.listen(3000, () => console.log("listening on port 3000"));
 
@@ -30,6 +45,6 @@ app.use("/", indexRouter);
 
 //Error 404
 
-app.use((req,res, next) => {
-    res.status(404).render("./main/error-404");
+app.use((req, res, next) => {
+  res.status(404).render("./main/error-404");
 });
