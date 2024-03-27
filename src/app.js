7232ff6191f16a67f91da40a8fe47c73d0eab2ae
MaTuTe248 <matutegnunez@com>
2024-03-27 20:39:53 -0300

    m

--- src/app.js
+++ src/app.js
@@ -21,6 +21,12 @@ app.use(session({
 app.use(cookieParser());
 app.use(loginMid);
 
+app.use((req, res, next)=>{
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+})
 app.listen(3000, () => console.log("listening on port 3000"));
 
 app.set("view engine", "ejs");
