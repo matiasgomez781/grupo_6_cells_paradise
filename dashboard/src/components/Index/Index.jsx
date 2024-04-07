d5759361522352c010f92e15cefdf5f28c281ae0
ElisOre <elias.orellana.31@gmail.com>
2024-04-07 01:57:05 -0300

    feat: componente de todas las marcas

--- dashboard/src/components/Index/Index.jsx
+++ dashboard/src/components/Index/Index.jsx
@@ -1,11 +1,6 @@
-import React, { Component } from "react";
-import ofertaBlack from "../../../../public/images/products/Oferta-BlackFriday.jpg";
-import ofertAcc from "../../../../public/images/products/Oferta-Accesorios.jpg";
-import Accesorios from "../Accesorios/Accesorios";
-import Celulares from "../Celulares/Celulares";
 import Product from "../Products/Products";
-import { Route, Routes } from "react-router-dom";
 import User from "../Users/User";
+import Brand from "../Brands/Brands";
 
 export default function Index() {
   return (
@@ -13,6 +8,7 @@ export default function Index() {
       <main className="content-wrap">
         <Product />
         <User />
+        <Brand />
       </main>
     </>
   );
