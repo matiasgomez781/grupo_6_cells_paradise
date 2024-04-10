c863b26ba0a219049a31c954488a048527c1c008
ElisOre <elias.orellana.31@gmail.com>
2024-04-10 14:38:11 -0300

    feat: routes aplicadas

--- dashboard/src/components/Index/Index.jsx
+++ dashboard/src/components/Index/Index.jsx
@@ -1,9 +1,13 @@
+import ContenidoMenu from "../ContenidoMenu/ContenidoMenu";
 import MenuWrap from "../MenuWrap/MenuWrap";
+import SearchWrap from "../SearchWrap/SearchWrap";
 
 export default function Index() {
   return (
     <>
       <main className="content-wrap">
+        <SearchWrap />
+        <ContenidoMenu />
         <MenuWrap />
       </main>
     </>
