ebc66dd5ec7e8af67cb490c994ff511f011fb96d
ElisOre <elias.orellana.31@gmail.com>
2024-04-10 18:16:04 -0300

    chore: borrar íconos no correspondientes

--- dashboard/src/components/ContenidoMenu/ContenidoMenu.jsx
+++ dashboard/src/components/ContenidoMenu/ContenidoMenu.jsx
@@ -14,46 +14,22 @@ export default function ContenidoMenu() {
           <h3>Opciones</h3>
           <ul>
             <li>
-              <Link to="/products">
-                <i
-                  className="bi bi-film"
-                  style={{ fontSize: "1.2rem", color: "cornflowerblue" }}
-                ></i>
-                - Productos
-              </Link>
+              <Link to="/products">Productos</Link>
             </li>
             <li>
-              <Link to="/categories">
-                <i
-                  className="bi bi-play-circle"
-                  style={{ fontSize: "1.2rem", color: "cornflowerblue" }}
-                ></i>
-                - Categorias
-              </Link>
+              <Link to="/categories">Categorias</Link>
             </li>
             <li>
-              <Link to="/brands">
-                <i
-                  className="bi bi-person"
-                  style={{ fontSize: "1.2rem", color: "cornflowerblue" }}
-                ></i>
-                - Marcas
-              </Link>
+              <Link to="/brands">Marcas</Link>
             </li>
             <li>
-              <Link to="/onSale">
-                <i className="bi bi-bar-chart"></i>- Mejores descuentos
-              </Link>
+              <Link to="/onSale">Mejores descuentos</Link>
             </li>
             <li>
-              <Link to="/users">
-                <i className="bi bi-tags"></i>- Todos los usuarios
-              </Link>
+              <Link to="/users">Todos los usuarios</Link>
             </li>
             <li>
-              <Link to="/lastUser">
-                <i className="bi bi-graph-up"></i>- Ultimo usuario
-              </Link>
+              <Link to="/lastUser">Ultimo usuario</Link>
             </li>
           </ul>
         </section>
